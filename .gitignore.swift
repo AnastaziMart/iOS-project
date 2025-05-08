3eb9104f5e8ec620e887797fa7100007f918a97b
Anastazi <anastasiamartus474@gmail.com>
2025-05-08 16:06:20 +0200

    Add .gitignore to exclude unnecessary files

---
+++ .gitignore.swift
@@ -0,0 +1,50 @@
+# Xcode
+build/
+DerivedData/
+*.pbxuser
+!default.pbxuser
+*.mode1v3
+!default.mode1v3
+*.mode2v3
+!default.mode2v3
+*.perspectivev3
+!default.perspectivev3
+xcuserdata/
+*.xcscmblueprint
+
+# macOS
+.DS_Store
+
+# SwiftPM
+.build/
+Packages/
+Package.resolved
+
+# CocoaPods
+Pods/
+Podfile.lock
+
+# Carthage
+Carthage/Build/
+
+# Fastlane
+fastlane/report.xml
+fastlane/Preview.html
+fastlane/screenshots/**/*.png
+
+# Archives
+*.xcarchive
+
+# Playgrounds
+timeline.xctimeline
+playground.xcworkspace
+
+# Unit test xcode logs
+*.xcresult
+//
+//  .gitignore.swift
+//  FirstApp
+//
+//  Created by Anastasiia Martus on 08/05/2025.
+//
+
